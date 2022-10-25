823e30be09345f4b9e277c783c85d524ae63b5ff
Kanghee Han <hcsy1219@gmail.com>
2022-10-25 23:49:03 +0900

    test: Check access database

--- src/main/java/practice/spring_mysql/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package practice.spring_mysql;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
--- src/main/java/practice/spring_mysql/boardApplication.java
+++ src/main/java/practice/spring_mysql/boardApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMysqlApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMysqlApplication.class, args);
-	}
-
+public class boardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(boardApplication.class, args);
+    }
 }
