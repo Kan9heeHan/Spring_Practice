7a9c6eb66f9fa4ab9f1bd5cf6f1778399d01dd0f
Kanghee Han <hcsy1219@gmail.com>
2022-10-30 23:55:54 +0900

    feat: Add post registration page

--- src/main/java/practice/spring_mysql/domain/post/PostController.java
+++ src/main/java/practice/spring_mysql/domain/post/PostController.java
@@ -4,16 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
     @GetMapping("/post/write.do")
-    public String openPostWrite(Model model){
-        model.addAttribute("t","title");
-        model.addAttribute("c","content");
-        model.addAttribute("w","writer");
-        return "post/write";
+    public String openPostWrite(@RequestParam(value="id", required = false) final Long id,Model model){
+        if(id!=null){
+            PostResponse post=postService.findPostById(id);
+            model.addAttribute("post",post);
+        }
+        return "/post/write";
+    }
+
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params){
+        postService.savePost(params);
+        return "redirect:/post/list.do";
     }
 }
