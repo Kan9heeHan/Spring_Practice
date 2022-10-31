eedcef924ff8896bfb2780b5797792e61f9a130e
Kanghee Han <hcsy1219@gmail.com>
2022-10-31 23:59:06 +0900

    feat: Add post view function

--- src/main/java/practice/spring_mysql/domain/post/PostController.java
+++ src/main/java/practice/spring_mysql/domain/post/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class PostController {
@@ -17,12 +19,40 @@ public class PostController {
             PostResponse post=postService.findPostById(id);
             model.addAttribute("post",post);
         }
-        return "/post/write";
+        return "post/write";
     }
 
     @PostMapping("/post/save.do")
+    window.onload = () => {
+        renderPostInfo();
+    }
+    function renderPostInfo(){
+        const post=[[${post}]];
+        if(!post){return false;}
+        const form = document.getElementById('saveForm');
+        const fields = ['id', 'title', 'content', 'writer', 'noticeYn'];
+        form.isNotice.checked = post.noticeYn;
+
+        fields.forEach(field => {
+                form[field].value = post[field];
+        })
+    }
     public String savePost(final PostRequest params){
         postService.savePost(params);
         return "redirect:/post/list.do";
+    }
+
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model){
+        List<PostResponse> posts=postService.findAllPost();
+        model.addAttribute("posts",posts);
+        return "post/list";
+    }
+
+    @GetMapping("/post/view.do")
+    public String openPostView(@RequestParam final Long id ,Model model){
+        PostResponse post= postService.findPostById(id);
+        model.addAttribute("post",post);
+        return "post/view";
     }
 }
